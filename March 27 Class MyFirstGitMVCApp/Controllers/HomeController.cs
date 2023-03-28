cea91c82c7da297946370513d533262d31c2ae1f
Elana Ades <elanaades1@gmail.com>
2023-03-27 21:23:16 -0400

    Changed some code.

--- March 27 Class MyFirstGitMVCApp/Controllers/HomeController.cs
+++ March 27 Class MyFirstGitMVCApp/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace March_27_Class_MyFirstGitMVCApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
