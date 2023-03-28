dd6355e74acb582b96ecff2db527d5cc31d04e96
Elana Ades <elanaades1@gmail.com>
2023-03-27 21:19:56 -0400

    Add project files.

---
+++ March 27 Class MyFirstGitMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace March_27_Class_MyFirstGitMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
